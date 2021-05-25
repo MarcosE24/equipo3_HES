0e737e17363d0b527e800dc7c0a654451fa59bfd
nelsonbogado99 <nelosonbrizue99@gmail.com>
2021-05-24 23:15:35 -0400

    Se agrego registro Tambien Reservacion de consulta

---
+++ Sistema_HES/Consultas.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Sistema_HES
+{
+    partial class Consultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbxNombre = new System.Windows.Forms.TextBox();
+            this.TbxApellido = new System.Windows.Forms.TextBox();
+            this.TbxDoctor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TbxFecha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnReservar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TbxConsultar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TbxNombre
+            // 
+            this.TbxNombre.Location = new System.Drawing.Point(217, 120);
+            this.TbxNombre.Name = "TbxNombre";
+            this.TbxNombre.Size = new System.Drawing.Size(189, 20);
+            this.TbxNombre.TabIndex = 0;
+            // 
+            // TbxApellido
+            // 
+            this.TbxApellido.Location = new System.Drawing.Point(217, 163);
+            this.TbxApellido.Name = "TbxApellido";
+            this.TbxApellido.Size = new System.Drawing.Size(189, 20);
+            this.TbxApellido.TabIndex = 1;
+            // 
+            // TbxDoctor
+            // 
+            this.TbxDoctor.Location = new System.Drawing.Point(217, 213);
+            this.TbxDoctor.Name = "TbxDoctor";
+            this.TbxDoctor.Size = new System.Drawing.Size(189, 20);
+            this.TbxDoctor.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre del Doctor :";
+            // 
+            // TbxFecha
+            // 
+            this.TbxFecha.Location = new System.Drawing.Point(217, 262);
+            this.TbxFecha.Name = "TbxFecha";
+            this.TbxFecha.Size = new System.Drawing.Size(189, 20);
+            this.TbxFecha.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(89, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(425, 42);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Reservacion de Consulta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(80, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Fecha de consulta:";
+            // 
+            // BtnReservar
+            // 
+            this.BtnReservar.Font = new System.Drawing.Font("Bahnschrift SemiBold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReservar.Location = new System.Drawing.Point(104, 374);
+            this.BtnReservar.Name = "BtnReservar";
+            this.BtnReservar.Size = new System.Drawing.Size(106, 37);
+            this.BtnReservar.TabIndex = 10;
+            this.BtnReservar.Text = "Reservar";
+            this.BtnReservar.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Font = new System.Drawing.Font("Bahnschrift SemiBold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelar.Location = new System.Drawing.Point(312, 374);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(94, 37);
+            this.BtnCancelar.TabIndex = 11;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(74, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Consultar con un/a:";
+            // 
+            // TbxConsultar
+            // 
+            this.TbxConsultar.Location = new System.Drawing.Point(217, 305);
+            this.TbxConsultar.Name = "TbxConsultar";
+            this.TbxConsultar.Size = new System.Drawing.Size(189, 20);
+            this.TbxConsultar.TabIndex = 13;
+            // 
+            // Consultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TbxConsultar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnReservar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TbxFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TbxDoctor);
+            this.Controls.Add(this.TbxApellido);
+            this.Controls.Add(this.TbxNombre);
+            this.Name = "Consultas";
+            this.Text = "Consultas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbxNombre;
+        private System.Windows.Forms.TextBox TbxApellido;
+        private System.Windows.Forms.TextBox TbxDoctor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TbxFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnReservar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TbxConsultar;
+    }
+}
--- Sistema_HES/Registro.Designer.cs
+++ Sistema_HES/Registro.Designer.cs
@@ -30,31 +30,194 @@ namespace Sistema_HES
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TbxNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TbxApellido = new System.Windows.Forms.TextBox();
+            this.TbxCi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TbxContraseña = new System.Windows.Forms.TextBox();
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TbxCorreo = new System.Windows.Forms.TextBox();
+            this.TbxTelefono = new System.Windows.Forms.TextBox();
+            this.TbxDireccion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(65, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nombre:";
             // 
-            // textBox1
+            // TbxNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.TbxNombre.Location = new System.Drawing.Point(219, 59);
+            this.TbxNombre.Name = "TbxNombre";
+            this.TbxNombre.Size = new System.Drawing.Size(236, 20);
+            this.TbxNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "REGISTRO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Documento N°:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(142, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Apellido:";
+            // 
+            // TbxApellido
+            // 
+            this.TbxApellido.Location = new System.Drawing.Point(219, 97);
+            this.TbxApellido.Name = "TbxApellido";
+            this.TbxApellido.Size = new System.Drawing.Size(236, 20);
+            this.TbxApellido.TabIndex = 5;
+            // 
+            // TbxCi
+            // 
+            this.TbxCi.Location = new System.Drawing.Point(219, 128);
+            this.TbxCi.Name = "TbxCi";
+            this.TbxCi.Size = new System.Drawing.Size(236, 20);
+            this.TbxCi.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(119, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Contraseña:";
+            // 
+            // TbxContraseña
+            // 
+            this.TbxContraseña.Location = new System.Drawing.Point(219, 271);
+            this.TbxContraseña.Name = "TbxContraseña";
+            this.TbxContraseña.Size = new System.Drawing.Size(236, 20);
+            this.TbxContraseña.TabIndex = 8;
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.Location = new System.Drawing.Point(125, 330);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(82, 35);
+            this.BtnAceptar.TabIndex = 9;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelar.Location = new System.Drawing.Point(373, 330);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(82, 35);
+            this.BtnCancelar.TabIndex = 10;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(73, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Correo electronico:";
+            // 
+            // TbxCorreo
+            // 
+            this.TbxCorreo.Location = new System.Drawing.Point(219, 164);
+            this.TbxCorreo.Name = "TbxCorreo";
+            this.TbxCorreo.Size = new System.Drawing.Size(236, 20);
+            this.TbxCorreo.TabIndex = 12;
+            // 
+            // TbxTelefono
+            // 
+            this.TbxTelefono.Location = new System.Drawing.Point(219, 199);
+            this.TbxTelefono.Name = "TbxTelefono";
+            this.TbxTelefono.Size = new System.Drawing.Size(236, 20);
+            this.TbxTelefono.TabIndex = 13;
+            // 
+            // TbxDireccion
+            // 
+            this.TbxDireccion.Location = new System.Drawing.Point(219, 234);
+            this.TbxDireccion.Name = "TbxDireccion";
+            this.TbxDireccion.Size = new System.Drawing.Size(236, 20);
+            this.TbxDireccion.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(62, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(144, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Numero de Telefono:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(133, 236);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 18);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Direccion:";
             // 
             // Registro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 377);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(715, 397);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TbxDireccion);
+            this.Controls.Add(this.TbxTelefono);
+            this.Controls.Add(this.TbxCorreo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnAceptar);
+            this.Controls.Add(this.TbxContraseña);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TbxCi);
+            this.Controls.Add(this.TbxApellido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TbxNombre);
             this.Controls.Add(this.label1);
             this.Name = "Registro";
             this.Text = "Registro";
@@ -66,6 +229,21 @@ namespace Sistema_HES
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TbxNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TbxApellido;
+        private System.Windows.Forms.TextBox TbxCi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TbxContraseña;
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TbxCorreo;
+        private System.Windows.Forms.TextBox TbxTelefono;
+        private System.Windows.Forms.TextBox TbxDireccion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
